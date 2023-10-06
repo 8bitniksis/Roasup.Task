29fc8fab36a7532fe750e510b6123be8953e60a8
Nikita <zalesov.anatoly@yandex.ru>
2023-10-06 18:04:59 +0400

    Init project

---
+++ Assets/Scripts/DrawWithMouse.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawWithMouse : MonoBehaviour
+{
+    [SerializeField]
+    private float minDistance = 0.1f;
+    public LineRenderer line;
+    private Vector3 previousPosition;
+    
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 1;
+        previousPosition = transform.position;
+    }
+
+    public void StartLine(Vector2 position)
+    {
+        line.positionCount = 1;
+        line.SetPosition(0, position);
+    }
+     
+    public void UpdateLines()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            currentPosition.z = 0.0f;
+
+            if(Vector3.Distance(currentPosition, previousPosition) > minDistance)
+            {
+                if(previousPosition == transform.position)
+                {
+                    line.SetPosition(0, currentPosition);
+                }
+                else
+                {
+                    line.positionCount++;
+                    line.SetPosition(line.positionCount - 1, currentPosition);
+                }
+                
+                previousPosition = currentPosition;
+            }
+        }
+    }
+}
